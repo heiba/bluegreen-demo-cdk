0a01bb73e879f54833bc8f986ab638f3e825ebe9
Mohamed Heiba <mohamed.adel.heiba@gmail.com>
2022-06-17 14:17:02 +0200

    First commit

---
+++ test/blue_green_demo_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BlueGreenDemoCdk from '../lib/blue_green_demo_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/blue_green_demo_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BlueGreenDemoCdk.BlueGreenDemoCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
